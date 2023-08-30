cd6132890f0231bebe0535e5f92342e6326bf2be
sergiohenrique10 <sergio_log@hotmail.com>
2023-08-30 16:29:24 -0300

    primeiro projeto node

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const uuid = require ("uuid") // cria numeros de ids aleatorios
+const app = express() // alterando o nome express para app
+app.use(express.json())
+
+
+
+const users =[]  
+
+const checkUserId = ( request, response, next) => { // middleware é uma função que recebe os parametros request, response e next
+    const {id} = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({error: "user not found"})
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next ()
+}
+
+app.get("/users", (request, response)=>{ // aplicação padrao node, rota.
+    return response.json(users)
+}) 
+
+app.post("/users", (request, response)=>{ // aplicação padrao node, rota.
+    const {name, age} = request.body
+
+    const user ={id: uuid.v4(), name, age}
+    
+    users.push(user)
+    return response.status(201).json(user)
+}) 
+
+app.put("/users/:id", checkUserId, (request, response)=>{ // aplicação padrao node, rota.
+    
+    const {name, age} = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updateUser = {id, name, age}
+ 
+    users[index] = updateUser
+
+    return response.json(updateUser)
+}) 
+
+app.delete("/users/:id", checkUserId, (request, response)=>{ // aplicação padrao node, rota.
+    const index = request.userIndex
+ 
+    users.splice(index,1)
+    return response.status(204).json()
+})
+
+app.listen(3000, () =>{
+    console.log("Server started on port 3000")
+}) // porta do servidor
+
